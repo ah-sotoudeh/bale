f66348248b27d292bc8a9404d4fd88a8980eae87
ah-sotoudeh <ah.sotoudeh@outlook.com>
2026-08-12 01:02:23 +0330

    Mount miniapp routes

--- bale_site/urls.py
+++ bale_site/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('channels_app.urls')),
     path('api/', include('orders.urls')),
+    path('miniapp/', include('miniapp.urls')),
 ]
