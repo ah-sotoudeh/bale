be07af161716ccff140b2f3031dbc7bfe73ca497
ah-sotoudeh <ah.sotoudeh@outlook.com>
2026-08-12 12:25:30 +0330

    Expose /bot/setup-migrate/ and /bot/setup-env/

--- bale_site/urls.py
+++ bale_site/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 
+from bot_flow.setup_views import env_check, run_migrate
 from bot_flow.webhook import bale_webhook
 
 
@@ -16,6 +17,8 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('bot/webhook/', bale_webhook, name='bale_webhook'),
+    path('bot/setup-migrate/', run_migrate, name='setup_migrate'),
+    path('bot/setup-env/', env_check, name='setup_env'),
     path('api/', include('channels_app.urls')),
     path('api/', include('orders.urls')),
     path('miniapp/', include('miniapp.urls')),
